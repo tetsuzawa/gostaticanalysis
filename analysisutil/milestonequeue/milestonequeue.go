04908daa5133e67b64c76e33df5bdd8f7105db34
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2020-09-04 01:29:30 +0900

    Change methods to public

--- analysisutil/milestonequeue/milestonequeue.go
+++ analysisutil/milestonequeue/milestonequeue.go
@@ -4,15 +4,27 @@ import "go/types"
 
 type MilestoneQueue []types.Object
 
-func (q *MilestoneQueue) push(t types.Object) {
+func (q *MilestoneQueue) Push(t types.Object) {
 	*q = append(*q, t)
 }
 
-func (q *MilestoneQueue) pop() (t types.Object) {
+func (q *MilestoneQueue) Pop() (t types.Object) {
 	if q == nil {
 		return nil
 	}
 	t = (*q)[0]
 	*q = (*q)[1:]
 	return t
+}
+
+func (q *MilestoneQueue) Head() types.Object {
+	return (*q)[0]
+}
+
+func (q *MilestoneQueue) Tail() types.Object {
+	return (*q)[(*q).Len()-1]
+}
+
+func (q *MilestoneQueue) Len() int {
+	return len(*q)
 }
